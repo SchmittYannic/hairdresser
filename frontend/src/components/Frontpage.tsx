e6a56cd7e708f94cd29ba0740083dc299326c211
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-04-20 17:46:38 +0200

    Adds contact-section to Frontpage

--- frontend/src/components/Frontpage.tsx
+++ frontend/src/components/Frontpage.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { FaPhone } from "react-icons/fa6"
 
 const Frontpage = () => {
     return (
@@ -20,6 +21,60 @@ const Frontpage = () => {
                             </div>
                         </div>
                     </div>
+
+                    <div id="contact-section" className="row">
+                        <div className="container container-fixed">
+                            <div className="col col-sm-12">
+                                <div className="module text" style={{marginBottom: "5px"}}>
+                                    <p className="darkspottext text-center">
+                                        Schön, dass Sie uns besuchen!
+                                    </p>
+                                </div>
+
+                                <div className="module text">
+                                    <h1 className="preamble text-center">
+                                        <span className="bold">
+                                            hairdresser
+                                        </span>
+                                        - Ihr Friseur in Würzburg
+                                    </h1>
+                                </div>
+
+                                <div className="module divider"></div>
+
+                                <div className="module text">
+                                    <p className="text-center">
+                                        Lernen Sie uns persönlich kennen und vereinbaren Sie gleich einen Termin.
+                                    </p>
+                                    <p className="text-center">&nbsp;</p>
+                                    <p className="text-center">
+                                        <span className="secondary-color">
+                                            <FaPhone />                                         
+                                        </span>
+                                        &nbsp;
+                                        <span className="secondary-color">
+                                            <a href="tel:000000000">
+                                                <span className="monoglobalWrap">
+                                                    00000 0000
+                                                </span>
+                                            </a>
+                                        </span>
+                                    </p>
+                                    <p className="text-center">&nbsp;</p>
+                                </div>
+
+                                <div className="module text">
+                                    <p className="bodytext text-center">
+                                        Wir freuen uns auf Sie!
+                                    </p>
+                                    <p className="bodytext text-center">&nbsp;</p>
+                                    <p className="bodytext text-center">
+                                        Ihr Team von hairdresser
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
                 </div>
             </div>
         </main>
