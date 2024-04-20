a83797edf8f618ead700dbfad9abb12e587d17ab
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-04-20 17:11:43 +0200

    Adds team picture to Frontpage

--- frontend/src/components/Frontpage.tsx
+++ frontend/src/components/Frontpage.tsx
@@ -1,7 +1,15 @@
 
 const Frontpage = () => {
     return (
-        <div>Frontpage</div>
+        <main id="frontpage" className="row designRow">
+            <div className="container w-full">
+                <div className="col col-sm-12">
+                    <a href="https://www.freepik.com/free-photo/front-view-young-friends-hugging_5965451.htm" target="_blank" title="Image by freepik">
+                        <div id="team-img" className="row"></div>
+                    </a>
+                </div>
+            </div>
+        </main>
     )
 }
 
