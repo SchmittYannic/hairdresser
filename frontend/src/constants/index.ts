bd780b768ca2795e37a51958aded279644dfb681
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-04-21 22:36:08 +0200

    Adds constants

---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,34 @@
+import {
+    kevinmurphyproduct,
+    kevinmurphyproduct2,
+    hairproducts1,
+    hairproducts2,
+} from "../assets"
+import { SliderImgsType } from "../components/ImageSlider";
+
+export const bookingpageSliderImgs: SliderImgsType[] = [
+    {
+        id: 0,
+        src: kevinmurphyproduct,
+        alt: "Kevin Murphy Produkte 1",
+        title: "",
+    },
+    {
+        id: 1,
+        src: kevinmurphyproduct2,
+        alt: "Kevin Murphy Produkte 2",
+        title: "",
+    },
+    {
+        id: 2,
+        src: hairproducts1,
+        alt: "Frontrow Produkte",
+        title: "",
+    },
+    {
+        id: 3,
+        src: hairproducts2,
+        alt: "Act+ Acre Produkte",
+        title: "",
+    },
+];
