402252c972ef6bdd7f092a019c6216d3a24b9bbb
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-04-22 19:25:28 +0200

    Adds second section to Stylingpage

--- frontend/src/components/Stylingpage.tsx
+++ frontend/src/components/Stylingpage.tsx
@@ -17,6 +17,39 @@ const Stylingpage = () => {
                             </div>
                         </div>
                     </div>
+
+                    <div id="r1427" className="row">
+                        <div className="container container-fixed">
+                            <div className="col col-sm-12">
+                                <div id="m2362" className="module text">
+                                    <h1 className="darkspottext text-center">
+                                        Styling bei hairdresser
+                                    </h1>
+                                </div>
+                                <div className="module text">
+                                    <h2 className="preamble text-center">
+                                        <span className="bold">
+                                            Man wird sich nach Ihnen umdrehen!
+                                        </span>
+                                    </h2>
+                                </div>
+                                <div id="m2618" className="module divider"></div>
+                                <div className="module text">
+                                    <p className="text-center">
+                                        <span className="bold">
+                                            Ob kurz oder lang, lockig oder glatt -&nbsp;
+                                        </span>
+                                        wir zaubern Ihre Traumfrisur.
+                                    </p>
+                                    <p className="text-center">&nbsp;</p>
+                                    <p className="text-center">
+                                        Sie stehen bei uns im Mittelpunkt. In unserer professionellen Typberatung besprechen wir Ihre Vorstellungen und Wünsche. Unseren Friseursalon verlassen Sie erst, wenn wir Sie glücklich gemacht haben.
+                                    </p>
+                                    <p className="text-center">&nbsp;</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
