7c40264576aacb3cef5bda8613ee5b3fb5bdc684
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-04-23 17:48:02 +0200

    Adds second section to Colourpage

--- frontend/src/components/Colourpage.tsx
+++ frontend/src/components/Colourpage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import ImageSlider from "./ImageSlider"
 import useWindowSize from "../hooks/useWindowSize"
 import { colourpageSliderImgs } from "../constants"
@@ -19,6 +20,60 @@ const Colourpage = () => {
                                     imgs={colourpageSliderImgs}
                                     amountperpage={sliderImgsPerPage}
                                 />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="r2751" className="row">
+                        <div className="container container-fixed">
+                            <div className="col col-sm-12">
+                                <div id="m4729" className="module text">
+                                    <h1 className="darkspottext text-center">
+                                        Farbe bei hairdresser
+                                    </h1>
+                                </div>
+                                <div className="module text">
+                                    <h2 className="preamble text-center">
+                                        <strong>
+                                            Wir erzielen glänzend schöne Farbergebnisse
+                                        </strong>
+                                    </h2>
+                                </div>
+                                <div id="m2314" className="module divider"></div>
+                                <div className="module text">
+                                    <p className="text-center">
+                                        <span className="bold">
+                                            Ob leuchtende Strähnen, natürliche Farbreflexe, blond oder rosa ... für Sie finden wir den Look, der zu Ihnen passt.
+                                        </span>
+                                    </p>
+                                    <p className="text-center">&nbsp;</p>
+                                    <p className="text-center">
+                                        Wir bieten Ihnen Luxus für Ihr Haar - wir verwenden ausschließlich hochwertige Farben, die schonend Ihre Haare färben und auch für sensible Kopfhaut geeignet sind. Dabei setzen wir auf die aktuelle&nbsp;
+                                        <a
+                                            className="tertiary-color"
+                                            href="http://colormebykm.com/"
+                                            target="_blank"
+                                        >
+                                            COLOR.ME
+                                        </a>
+                                        &nbsp;Spitzenhaarfarbe von&nbsp;
+                                        <a
+                                            className="tertiary-color"
+                                            href="http://de.kevinmurphy.com.au/"
+                                            target="_blank"
+                                        >
+                                            KEVIN.MURPHY
+                                        </a>
+                                        . Erfahren Sie hier mehr über die&nbsp;
+                                        <Link
+                                            className="tertiary-color"
+                                            to={"/salon"}
+                                        >
+                                            Produkte
+                                        </Link>
+                                        , die wir verwenden. Das ist Luxus für Ihr Haar.
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
