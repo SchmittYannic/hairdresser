c1c15540d32874079e4abc98cd83f4592b519773
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-04-23 18:05:05 +0200

    Adds third section to Colourpage

--- frontend/src/components/Colourpage.tsx
+++ frontend/src/components/Colourpage.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom"
+import { FaInstagram } from "react-icons/fa"
 import ImageSlider from "./ImageSlider"
 import useWindowSize from "../hooks/useWindowSize"
+import { vorhernachher } from "../assets"
 import { colourpageSliderImgs } from "../constants"
 import "./Colourpage.scss"
 
@@ -74,6 +76,64 @@ const Colourpage = () => {
                                         , die wir verwenden. Das ist Luxus für Ihr Haar.
                                     </p>
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="instagram-section-colourpage" className="row">
+                        <div className="container w-full">
+                            <a
+                                id="instagramImg-colourpage"
+                                className="col col-md-6 col-lg-6 col-sm-12"
+                                href="https://www.freepik.com/free-photo/hairdresser-using-electric-trimmer-cutting-client39s-hair_27258635.htm#fromView=search&page=1&position=14&uuid=c0e9aa56-ca74-497d-a564-6d42eedc1330"
+                                target="_blank"
+                                title="Image by freepik"
+                            ></a>
+                            <div
+                                id="instagramText-colourpage"
+                                className="col col-md-6 col-lg-6 col-sm-12"
+                            >
+                                <div className="module text">
+                                    <h2 className="headline">
+                                        Colour
+                                    </h2>
+                                </div>
+                                <div id="m2970" className="module text">
+                                    <p>
+                                        Klicken Sie sich doch gerne durch unsere&nbsp;
+                                        <span className="bold">
+                                            Instagram-Galerie.
+                                        </span>
+                                        &nbsp;Vielleicht erscheint auch bald Ihr Bild hier, wenn wir Sie happy gemacht haben.
+                                    </p>
+                                    <p className="bodytext">&nbsp;</p>
+                                    <p className="bodytext">
+                                        <strong>
+                                            Überzeugen Sie sich von unserem Können!
+                                        </strong>
+                                    </p>
+                                </div>
+                                <a
+                                    id="m1056"
+                                    className="module button"
+                                    href="https://www.instagram.com"
+                                    target="_blank"
+                                    title="Instagramprofil öffnen"
+                                >
+                                    <span className="icon-container">
+                                        <FaInstagram aria-hidden />
+                                    </span>
+                                    &nbsp;
+                                    <span>
+                                        Zum Instagram-Profil
+                                    </span>
+                                </a>
+                                <img
+                                    id="vorhernachherImg-colourpage"
+                                    className="module image"
+                                    src={vorhernachher}
+                                    alt="vorhernachher"
+                                />
                             </div>
                         </div>
                     </div>
