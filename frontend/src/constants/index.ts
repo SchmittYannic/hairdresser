d770a7059861163d8971d4759c696decbedc2f98
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-04-24 22:03:36 +0200

    Adjusts constants to new ImageSlider component

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -13,136 +13,196 @@ import {
     haircolour3,
     selfcareproducts,
 } from "../assets"
-import { SliderImgsType } from "../components/ImageSlider";
 
-export const bookingpageSliderImgs: SliderImgsType[] = [
+type ImageSliderObjectType = {
+    id: number,
+    src: string,
+    alt: string,
+    href?: string,
+    title?: string,
+};
+
+const kevinmurphyproduct1Obj: Omit<ImageSliderObjectType, "id"> = {
+    src: kevinmurphyproduct,
+    alt: "Kevin Murphy Produkte 1",
+    href: "https://unsplash.com/de/fotos/zwei-weisse-und-schwarz-etikettierte-flaschen-EekAcoEhYGU",
+    title: "Foto von Nick Scott auf Unsplash",
+};
+
+const kevinmurphyproduct2Obj: Omit<ImageSliderObjectType, "id"> = {
+    src: kevinmurphyproduct2,
+    alt: "Kevin Murphy Produkte 2",
+    href: "https://unsplash.com/de/fotos/weisse-und-rosa-etikettierte-flaschen-Yo6tVaQHAho",
+    title: "Foto von Nick Scott auf Unsplash",
+};
+
+const hairproducts1Obj: Omit<ImageSliderObjectType, "id"> = {
+    src: hairproducts1,
+    alt: "Frontrow Produkte",
+    href: "https://unsplash.com/de/fotos/zwei-verschiedene-arten-von-haarprodukten-auf-rosa-hintergrund-mGEVuLZNAxY",
+    title: "Foto von Amy Lewis auf Unsplash",
+};
+
+const hairproducts2Obj: Omit<ImageSliderObjectType, "id"> = {
+    src: hairproducts2,
+    alt: "Act+ Acre Produkte",
+    href: "https://unsplash.com/de/fotos/weisser-und-grun-beschrifteter-weicher-schlauch-aYrOtqypmho",
+    title: "Foto von Glenna Haug auf Unsplash",
+};
+
+const hairstyle1Obj: Omit<ImageSliderObjectType, "id"> = {
+    src: hairstyle1,
+    alt: "Frau mit Locken",
+    href: "https://www.freepik.com/free-photo/beautiful-woman-with-curls-makeup_6048346.htm",
+    title: "Image by Racool_studio on Freepik",
+};
+
+const hairstyle2Obj: Omit<ImageSliderObjectType, "id"> = {
+    src: hairstyle2,
+    alt: "Hochzeitfrisur von hinten",
+    href: "https://www.freepik.com/free-photo/beauty-wedding-hairstyle-rear-view-isolated-white_10230952.htm",
+    title: "Image by valuavitaly on Freepik",
+};
+
+const hairstyle3Obj: Omit<ImageSliderObjectType, "id"> = {
+    src: hairstyle3,
+    alt: "Frau mit langen glatten Haaren",
+    href: "Image by valuavitaly on Freepik",
+    title: "https://www.freepik.com/free-photo/portrait-beautiful-sexy-woman-with-long-hair-fashion-model-with-straight-hairstyle_11961892.htm",
+};
+
+const hairproducts3Obj: Omit<ImageSliderObjectType, "id"> = {
+    src: hairproducts3,
+    alt: "Haarprodukte3",
+    href: "https://www.freepik.com/free-photo/small-plant-near-various-cosmetics-bottles_2146565.htm",
+    title: "Image by freepik",
+};
+
+const hairproducts4Obj: Omit<ImageSliderObjectType, "id"> = {
+    src: hairproducts4,
+    alt: "Haarprodukte4",
+    href: "Image by freepik",
+    title: "https://www.freepik.com/free-ai-image/3d-rendering-personal-care-products-fondant-pink_94940306.htm",
+};
+
+const selfcareproductsObj: Omit<ImageSliderObjectType, "id"> = {
+    src: selfcareproducts,
+    alt: "Haarstyling Zubehör",
+    href: "Image by freepik",
+    title: "https://www.freepik.com/free-photo/close-up-man-selfcare-products_23668485.htm",
+};
+
+const haircolour1Obj: Omit<ImageSliderObjectType, "id"> = {
+    src: haircolour1,
+    alt: "Frau mit lila Haarfarbe",
+    href: "https://unsplash.com/de/fotos/purple-haired-woman-in-black-top-leaning-on-wall-tLKOj6cNwe0",
+    title: "Foto von Kareya Saleh auf Unsplash",
+};
+
+const haircolour2Obj: Omit<ImageSliderObjectType, "id"> = {
+    src: haircolour2,
+    alt: "Frau mit gefärbten Haaren",
+    href: "https://unsplash.com/de/fotos/frau-in-schwarzer-jacke-mit-kragen-pZTVa_Gt1f8",
+    title: "Foto von Thibault Debaene auf Unsplash",
+};
+
+const haircolour3Obj: Omit<ImageSliderObjectType, "id"> = {
+    src: haircolour3,
+    alt: "Frau bekommt Haare gefärbt",
+    href: "https://unsplash.com/de/fotos/braunes-haar-mit-weissem-band-B1B-53ZBgng",
+    title: "Foto von Lera Kogan auf Unsplash",
+};
+
+export const bookingpageSliderImgs: ImageSliderObjectType[] = [
     {
         id: 0,
-        src: kevinmurphyproduct,
-        alt: "Kevin Murphy Produkte 1",
-        title: "",
+        ...kevinmurphyproduct1Obj,
     },
     {
         id: 1,
-        src: kevinmurphyproduct2,
-        alt: "Kevin Murphy Produkte 2",
-        title: "",
+        ...kevinmurphyproduct2Obj,
     },
     {
         id: 2,
-        src: hairproducts1,
-        alt: "Frontrow Produkte",
-        title: "",
+        ...hairproducts1Obj,
     },
     {
         id: 3,
-        src: hairproducts2,
-        alt: "Act+ Acre Produkte",
-        title: "",
+        ...hairproducts2Obj,
     },
 ];
 
-export const stylingpageSliderImgs: SliderImgsType[] = [
+export const stylingpageSliderImgs: ImageSliderObjectType[] = [
     {
         id: 0,
-        src: hairstyle1,
-        alt: "Frau mit Locken",
-        title: "",
+        ...hairstyle1Obj,
     },
     {
         id: 1,
-        src: hairstyle2,
-        alt: "Hochzeitfrisur von hinten",
-        title: "",
+        ...hairstyle2Obj,
     },
     {
         id: 3,
-        src: hairstyle3,
-        alt: "Frau mit langen glatten Haaren",
-        title: "",
+        ...hairstyle3Obj,
     },
 ];
 
-export const stylingpageSliderImgs2: SliderImgsType[] = [
+export const stylingpageSliderImgs2: ImageSliderObjectType[] = [
     {
         id: 0,
-        src: hairproducts3,
-        alt: "Haarprodukte3",
-        title: "",
+        ...hairproducts3Obj,
     },
     {
         id: 1,
-        src: hairproducts4,
-        alt: "Haarprodukte4",
-        title: "",
+        ...hairproducts4Obj,
     },
     {
         id: 3,
-        src: selfcareproducts,
-        alt: "Haarstyling Zubehör",
-        title: "",
+        ...selfcareproductsObj,
     },
 ];
 
-export const colourpageSliderImgs: SliderImgsType[] = [
+export const colourpageSliderImgs: ImageSliderObjectType[] = [
     {
         id: 0,
-        src: kevinmurphyproduct,
-        alt: "Kevin Murphy Produkte 1",
-        title: "",
+        ...kevinmurphyproduct1Obj,
     },
     {
         id: 1,
-        src: hairproducts3,
-        alt: "Haarprodukte3",
-        title: "",
+        ...hairproducts3Obj,
     },
     {
         id: 3,
-        src: kevinmurphyproduct2,
-        alt: "Kevin Murphy Produkte 2",
-        title: "",
+        ...kevinmurphyproduct2Obj,
     },
 ];
 
-export const colourpageSliderImgs2: SliderImgsType[] = [
+export const colourpageSliderImgs2: ImageSliderObjectType[] = [
     {
         id: 0,
-        src: haircolour1,
-        alt: "Frau mit lila Haarfarbe",
-        title: "",
+        ...haircolour1Obj,
     },
     {
         id: 1,
-        src: haircolour2,
-        alt: "Frau mit gefärbten Haaren",
-        title: "",
+        ...haircolour2Obj,
     },
     {
         id: 2,
-        src: haircolour3,
-        alt: "Frau bekommt Haare gefärbt",
-        title: "",
+        ...haircolour3Obj,
     },
 ];
 
-export const colourpageSliderImgs3: SliderImgsType[] = [
+export const colourpageSliderImgs3: ImageSliderObjectType[] = [
     {
         id: 0,
-        src: hairproducts4,
-        alt: "Haarprodukte4",
-        title: "",
+        ...hairproducts4Obj,
     },
     {
         id: 1,
-        src: selfcareproducts,
-        alt: "Haarstyling Zubehör",
-        title: "",
+        ...selfcareproductsObj,
     },
     {
         id: 2,
-        src: hairproducts1,
-        alt: "Frontrow Produkte",
-        title: "",
+        ...hairproducts1Obj,
     },
-]
+];
