8b2323b711444dcaafac1b5c946f538dae2cbe4f
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-04-28 23:09:21 +0200

    Adds ScrollToTop component
    Scrolls to top of page when path changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,17 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import ScrollToTop from "./components/ScrollToTop.tsx"
 import "./index.scss"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <Routes>
-                <Route path="/*" element={<App />} />
-            </Routes>
+            <ScrollToTop>
+                <Routes>
+                    <Route path="/*" element={<App />} />
+                </Routes>
+            </ScrollToTop>
         </BrowserRouter>
     </React.StrictMode>,
 )
