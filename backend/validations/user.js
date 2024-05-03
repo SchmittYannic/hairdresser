b9bc0e9d44065d54c26039320c45769ced197189
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-03 22:48:40 +0200

    Updates user validation

--- backend/validations/user.js
+++ backend/validations/user.js
@@ -26,15 +26,60 @@ const lastname = Joi.string().max(80).required().messages({
 });
 
 const firstname = Joi.string().max(80).required().messages({
-    "string.max": "Lastname cant be longer than 80 characters",
-    "string.empty": "Lastname is required",
-    "any.required": "Lastname is required",
+    "string.max": "Firstname cant be longer than 80 characters",
+    "string.empty": "Firstname is required",
+    "any.required": "Firstname is required",
 });
 
+const title = Joi.string()
+    .pattern(new RegExp(/^(Herr|Frau|Divers)$/))
+    .messages({
+        "string.pattern.base": "Title must be either Herr, Frau or Divers",
+        "string.empty": "Title is required",
+        "any.required": "Title is required",
+    })
+    .required()
+
+const birthday = Joi.date()
+    .less("now")
+    .required()
+    .messages({
+        "date.base": "Birthday is required",
+        "date.less": "Birthday cant be in the future",
+        "any.required": "Birthday is required",
+    })
+
+const phonenumber = Joi.string()
+    .pattern(new RegExp(/^\d+$/))
+    .message("Phonenumber must only consist of numbers")
+    .pattern(/^\d{7,15}$/)
+    .message("Phonenumber must be between 7 and 15 digits long")
+    .required()
+    .messages({
+        "any.required": "Phonenumber is required",
+    })
+
+const reminderemail = Joi.boolean().required().messages({
+    "any.required": "Reminder email optionfield is required"
+})
+
+const birthdayemail = Joi.boolean().required().messages({
+    "any.required": "Birthday email optionfield is required"
+})
+
+const newsletter = Joi.boolean().required().messages({
+    "any.required": "Newsletter optionfield is required"
+})
 
 export const register = Joi.object().keys({
     email,
     password,
+    title,
     lastname,
     firstname,
+    birthday,
+    phonenumber,
+    reminderemail,
+    birthdayemail,
+    newsletter,
 });
