177aee8b317e0ee3231f696f5a35cdd8e49e1d8d
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-04 22:38:52 +0200

    Adds queryclient and provider to Application

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,27 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
+import { QueryClient, QueryClientProvider, setLogger } from "react-query"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import App from "./App.tsx"
 import ScrollToTop from "./components/ScrollToTop.tsx"
+
+const queryClient = new QueryClient();
+
+setLogger({
+    log: () => { },
+    warn: () => { },
+    error: () => { },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
             <ScrollToTop>
-                <Routes>
-                    <Route path="/*" element={<App />} />
-                </Routes>
+                <QueryClientProvider client={queryClient}>
+                    <Routes>
+                        <Route path="/*" element={<App />} />
+                    </Routes>
+                </QueryClientProvider>
             </ScrollToTop>
         </BrowserRouter>
     </React.StrictMode>,
