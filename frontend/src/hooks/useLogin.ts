22492060dfdbca879c3d398cbeb86996b1d54861
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-08 20:44:21 +0200

    Adds useLogin hook

---
+++ frontend/src/hooks/useLogin.ts
@@ -0,0 +1,24 @@
+import { isAxiosError } from "axios"
+import { useMutation } from "react-query"
+import api from "../api"
+import { LoginDataType } from "../utils/types"
+
+const useLogin = () => {
+    const login = async (userData: LoginDataType) => {
+        const response = await api.post("/login", userData)
+        return response.data
+    }
+
+    return useMutation({
+        mutationFn: login,
+        onError: (error) => {
+            if (isAxiosError(error) && error.response) {
+                console.error(error.response.data.message)
+            } else {
+                console.error(error)
+            }
+        }
+    })
+}
+
+export default useLogin
