3c9764ffa5391be28fdc346bdc421f6d367f4bbb
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-08 21:19:22 +0200

    Adds max length requirement to Loginschema in frontend

--- frontend/src/validation/Loginschema.ts
+++ frontend/src/validation/Loginschema.ts
@@ -2,10 +2,13 @@ import * as Yup from "yup";
 
 const requiredMsg = "Bitte ausfüllen";
 
-const email = Yup.string().email("ungültiges Email-Format").required(requiredMsg);
+const email = Yup.string().email("ungültiges Email-Format")
+    .required(requiredMsg)
+    .max(80, "Email darf nicht mehr als 80 Zeichen lang sein");
 
 const password = Yup.string()
     .required(requiredMsg)
+    .max(80, "Passwort darf nicht mehr als 80 Zeichen lang sein");
 
 export default Yup.object().shape({
     email,
