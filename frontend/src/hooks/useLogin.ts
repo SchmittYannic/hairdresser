87bb42b8e2546cb134189c86d59890293ccde21a
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-08 20:53:20 +0200

    Updates post request url of useLogin

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -5,7 +5,7 @@ import { LoginDataType } from "../utils/types"
 
 const useLogin = () => {
     const login = async (userData: LoginDataType) => {
-        const response = await api.post("/login", userData)
+        const response = await api.post("/auth/login", userData)
         return response.data
     }
 
