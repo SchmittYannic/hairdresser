41653ff91aaa7ace091fa38f676bf11dd9c730a9
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-10 00:11:42 +0200

    Adds logout button and session timer to Booking component

--- frontend/src/features/booking/Booking.tsx
+++ frontend/src/features/booking/Booking.tsx
@@ -1,16 +1,34 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
+import { MdLogout } from "react-icons/md";
+import useSessionContext from "../../hooks/useSessionContext";
+import useLogout from "../../hooks/useLogout";
 import Login from "./Login";
 import Register from "./Register";
 import ResetPassword from "./ResetPassword";
 import AGB from "./AGB";
-import { logo } from "../../assets"
-import "./Booking.scss"
+import Dashboard from "./Dashboard";
+import { logo } from "../../assets";
+import "./Booking.scss";
 
-export type activeTabType = "login" | "register" | "reset" | "agb";
+export type activeTabType = "login" | "register" | "reset" | "agb" | "dashboard";
 
 const Booking = () => {
 
+    const { userInfo } = useSessionContext();
+    const { mutate } = useLogout();
     const [activeTab, setActiveTab] = useState<activeTabType>("login");
+
+    const handleLogoutClicked = () => {
+        mutate();
+    }
+
+    useEffect(() => {
+        if (userInfo.userId === "") {
+            setActiveTab("login")
+        } else {
+            setActiveTab("dashboard");
+        }
+    }, [userInfo])
 
     return (
         <div id="booking">
@@ -31,10 +49,33 @@ const Booking = () => {
                             {activeTab === "register" && "Registrierung"}
                             {activeTab === "reset" && "Passwort zurücksetzen"}
                             {activeTab === "agb" && "AGB und Datenschutzerklärung"}
+                            {activeTab === "dashboard" && "Terminübersicht"}
                         </span>
                         <span className="pageHeaderInfo">
                             {activeTab === "login" && "Hier können Sie Ihre nächsten Termine schnell und einfach online buchen - rund um die Uhr, auch am Wochenende."}
                         </span>
+                        <div className="sessionTimeoutLabelContainer">
+                            <span className="sessionTimeoutLabel">
+                                Logout in:
+                            </span>
+                        </div>
+                        {
+                            activeTab === "dashboard" &&
+                            <div className="headerButtonContainer">
+                                <button
+                                    className="inverseButton bookingButton logoutButton"
+                                    type="button"
+                                    onClick={handleLogoutClicked}
+                                >
+                                    <span className="icon-container">
+                                        <MdLogout aria-hidden />
+                                    </span>
+                                    <span className="icon-gap">
+                                        Abmelden
+                                    </span>
+                                </button>
+                            </div>
+                        }
                         <hr className="horizontal-ruler" />
                     </div>
                 </div>
@@ -43,6 +84,7 @@ const Booking = () => {
                     {activeTab === "reset" && <ResetPassword callback={setActiveTab} />}
                     {(activeTab === "register" || activeTab === "agb") && <Register activeTab={activeTab} callback={setActiveTab} />}
                     {(activeTab === "register" || activeTab === "agb") && <AGB activeTab={activeTab} callback={setActiveTab} />}
+                    {activeTab === "dashboard" && <Dashboard />}
                 </div>
                 <div className="bookingFooter"></div>
             </main>
