d88865866269c31434590a5b92173baebbb8dc4e
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-09 21:34:42 +0200

    Updates authController with sessions

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,40 +1,71 @@
 import bcrypt from "bcrypt"
 import User from "../models/User.js"
 import loginschema from "../validation/loginschema.js"
+import { sessionizeUser } from "../utils/helpers.js";
 
 // @desc login
-// @route POST /login
+// @route POST /
 // @access Public
 const login = async (req, res) => {
     try {
-        await loginschema.validateAsync(req.body);
-
         const { email, password } = req.body;
+        await loginschema.validateAsync({ email, password });
 
         const foundUser = await User.findOne({ email: email.toLowerCase() }).lean().exec();
 
         if (!foundUser) {
-            return res.status(401).json({ message: "E-Mail-Adresse ist nicht registriert", key: "email" });
+            return res.status(401).json({ message: "E-Mail-Adresse ist nicht registriert", context: { key: "email" } });
         }
 
         const match = await bcrypt.compare(password, foundUser.password);
 
         if (!match) {
-            return res.status(401).json({ message: "Ungültiges Passwort", key: "password" });
+            return res.status(401).json({ message: "Ungültiges Passwort", context: { key: "password" } });
         }
 
-        return res.status(200).json({ message: "login success" })
+        const sessionUser = sessionizeUser(foundUser)
+        req.session.user = sessionUser
+        console.log(req.session)
+
+        return res.status(200).json({ message: "login success", sessionUser })
     } catch (err) {
-        if (err.name === "ValidationError") {
-            const errMsg = err.details.reduce((acc, error) => acc + error.message, "");
-            const errKey = err.details.reduce((acc, error) => acc + error.context.key, "");
-            return res.status(400).json({ message: errMsg, key: errKey });
-        } else {
-            return res.status(400).json({ message: "Etwas ist schiefgelaufen. Versuchen Sie es später erneut." });
-        }
+        return res.status(400).send(parseError(err));
     }
+}
+
+// @desc logout
+// @route DELETE /
+// @access Public
+const logout = (req, res) => {
+    const { session } = req
+
+    try {
+        const user = session.user;
+        console.log(user)
+        if (user) {
+            session.destroy(err => {
+                if (err) throw (err);
+                res.clearCookie(process.env.SESS_NAME);
+                res.send(user);
+            });
+        } else {
+            throw new Error('Something went wrong');
+        }
+    } catch (err) {
+        res.status(422).send(parseError(err));
+    }
+}
+
+// @desc loggedIn
+// @route GET /
+// @access Public
+const loggedIn = (req, res) => {
+    const { session } = req
+    return res.status(200).json({ message: "ok", ...session.user });
 }
 
 export {
     login,
+    logout,
+    loggedIn,
 }
