ecabee0b2f6316b50d22a3f53c3304b0b670de22
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-11 00:18:28 +0200

    Adds EditUserType

--- frontend/src/utils/types.d.ts
+++ frontend/src/utils/types.d.ts
@@ -17,3 +17,18 @@ export type LoginDataType = {
     email: string,
     password: string,
 }
+
+export type EditUserType = {
+    title: string,
+    email: string,
+    oldpassword?: string,
+    password?: string,
+    passwordrepeat?: string,
+    lastname: string,
+    firstname: string,
+    birthday?: string,
+    phonenumber: string,
+    reminderemail: boolean,
+    birthdayemail: boolean,
+    newsletter: boolean,
+}
