68767a74eb8c652ab7a5999e384a3c11105ae965
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-10 02:27:02 +0200

    Adds requireAuth middle ware to /auth delete endpoint

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { login, logout, loggedIn } from "../controllers/authController.js";
+import requireAuth from "../middleware/requireAuth.js"
+
 const router = express.Router();
 
 router.route("/")
     .get(loggedIn)
     .post(login)
-    .delete(logout)
+    .delete(requireAuth, logout)
 
 export default router;
