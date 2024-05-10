4f65756b3889a40889d2347dd6322d43402657d4
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-10 03:34:39 +0200

    Fixes requireAuth middleware

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,8 +1,8 @@
 const requireAuth = (req, res, next) => {
-    if (!req.session || !req.session.userId) {
-        next(); // User is authenticated, continue to next middleware
+    if (req.session && req.session.user) {
+        next();
     } else {
-        res.status(401).json({ message: "Unauthorized" }); // User is not authenticated
+        res.status(401).json({ message: "Unauthorized" });
     }
 }
 
