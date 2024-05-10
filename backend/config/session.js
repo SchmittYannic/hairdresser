86b4ab9f2c2116a9678fd64113f96f8aad8970ed
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-11 00:14:22 +0200

    Updates sameSite value in sessionConfig

--- backend/config/session.js
+++ backend/config/session.js
@@ -12,7 +12,7 @@ const sessionConfig = (db) => {
             ttl: parseInt(process.env.SESS_LIFETIME) ?? 20 * 60 //time to life in seconds.
         }),
         cookie: {
-            sameSite: "strict",
+            sameSite: "none",
             secure: process.env.NODE_ENV === "production",
             maxAge: parseInt(process.env.SESS_LIFETIME) * 1000
         }
