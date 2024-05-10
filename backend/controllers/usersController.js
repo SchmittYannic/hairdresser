74acde7edfc2ca615f15b74d09a62b1f625e5497
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-11 00:15:34 +0200

    Adds updateUser to usersController

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import User from "../models/User.js";
-import userschema from "../validation/userschema.js";
+import { userschema, passwordschema, birthdayschema, emailschema, titleschema, lastnameschema, firstnameschema, phonenumberschema, reminderemailschema, birthdayemailschema, newsletterschema } from "../validation/userschema.js";
 import { sessionizeUser, parseError, birthdayToString } from "../utils/helpers.js";
 
 // @desc Create new user
@@ -86,6 +86,129 @@ const createNewUser = async (req, res) => {
     }
 };
 
+// @desc Update user
+// @route PATCH /users
+// @access Private
+const updateUser = async (req, res) => {
+    try {
+        const { userId } = req.session.user
+
+        const {
+            title,
+            email,
+            oldpassword,
+            password,
+            lastname,
+            firstname,
+            birthday,
+            phonenumber,
+            reminderemail,
+            birthdayemail,
+            newsletter,
+        } = req.body
+
+        const foundUser = await User.findById(userId).exec();
+
+        if (!foundUser) {
+            return res.status(400).json({ message: "Konnte Nutzer id nicht in Datenbank finden" });
+        }
+
+        //user wants to change password
+        if (password !== "" || oldpassword !== "") {
+            //user tries changing password without giving oldpassword
+            if (password !== "" && oldpassword === "") {
+                return res.status(400).json({ message: "Passwortänderung benötigt altes Passwort", context: { key: "oldpassword" } });
+            }
+            //user tries changing password without giving a new password
+            if (password === "" && oldpassword !== "") {
+                return res.status(400).json({ message: "Passwortänderung benötigt neues Passwort", context: { key: "password" } });
+            }
+            //see if password fits the schema
+            await passwordschema.validateAsync(password);
+            //see if oldpassword matches password in database
+            const match = await bcrypt.compare(oldpassword, foundUser.password);
+            if (!match) {
+                return res.status(401).json({ message: "Ungültiges Passwort", context: { key: "oldpassword" } });
+            }
+            //if matches hash new password
+            newHashedPw = await bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT_ROUNDS));
+            foundUser.password = newHashedPw
+        }
+
+        if (birthday !== "") {
+            const parts = birthday.split(".");
+            const birthdayAsDate = new Date(parseInt(parts[2], 10),
+                parseInt(parts[1], 10) - 1,
+                parseInt(parts[0], 10));
+            await birthdayschema.validateAsync(birthdayAsDate);
+            foundUser.birthday = birthdayAsDate;
+        }
+
+        if (email !== foundUser.email) {
+            await emailschema.validateAsync(email);
+            foundUser.email = email
+        }
+        if (title !== foundUser.title) {
+            await titleschema.validateAsync(title);
+            foundUser.title = title
+        }
+        if (lastname !== foundUser.lastname) {
+            await lastnameschema.validateAsync(lastname);
+            foundUser.lastname = lastname
+        }
+        if (firstname !== foundUser.firstname) {
+            await firstnameschema.validateAsync(firstname);
+            foundUser.firstname = firstname
+        }
+        if (phonenumber !== foundUser.phonenumber) {
+            await phonenumberschema.validateAsync(phonenumber);
+            foundUser.phonenumber = phonenumber
+        }
+        if (reminderemail !== foundUser.reminderemail) {
+            await reminderemailschema.validateAsync(reminderemail);
+            foundUser.reminderemail = reminderemail
+        }
+        if (birthdayemail !== foundUser.birthdayemail) {
+            await birthdayemailschema.validateAsync(birthdayemail);
+            foundUser.birthdayemail = birthdayemail
+        }
+        if (newsletter !== foundUser.newsletter) {
+            await newsletterschema.validateAsync(newsletter);
+            foundUser.newsletter = newsletter
+        }
+
+        const updateUser = await foundUser.save();
+
+        const sessionUser = sessionizeUser(updateUser);
+
+        req.session.touch(() => {
+            req.session.user = sessionUser;
+        })
+
+        const userInfo = {
+            ...sessionUser,
+            title: updateUser.title,
+            lastname: updateUser.lastname,
+            firstname: updateUser.firstname,
+            birthday: birthdayToString(updateUser.birthday),
+            phonenumber: updateUser.phonenumber,
+            reminderemail: updateUser.reminderemail,
+            birthdayemail: updateUser.birthdayemail,
+            newsletter: updateUser.newsletter,
+        }
+
+        const cookieInfo = {
+            cookie_expires: req.session.cookie._expires,
+            cookie_originalMaxAge: req.session.cookie.originalMaxAge,
+        }
+
+        return res.status(200).json({ message: "Änderung erfolgreich", userInfo, cookieInfo });
+    } catch (err) {
+        return res.status(400).send(parseError(err));
+    }
+}
+
 export {
     createNewUser,
+    updateUser,
 }
