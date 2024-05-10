5303c7da441cadda723c34b213b56f5398dfcc1c
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-11 00:16:42 +0200

    Adds updateUser to /users patch endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { createNewUser } from "../controllers/usersController.js";
+import { createNewUser, updateUser } from "../controllers/usersController.js";
+import requireAuth from "../middleware/requireAuth.js"
+
 const router = express.Router();
 
 router.route("/")
     .post(createNewUser)
+    .patch(requireAuth, updateUser)
 
 export default router;
