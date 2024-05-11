39d17d3cdff28d98b6bdb283e689d6093f248d42
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-12 00:48:18 +0200

    Updates content of Bookdate component

--- frontend/src/features/booking/Bookdate.tsx
+++ frontend/src/features/booking/Bookdate.tsx
@@ -1,7 +1,39 @@
+import useSessionContext from "../../hooks/useSessionContext";
 
 const Bookdate = () => {
+
+    const { setActiveTab } = useSessionContext();
+
+    const handleBackButtonClicked = () => {
+        setActiveTab("services");
+    };
+
     return (
-        <div>Bookdate</div>
+        <div className="page">
+            <div className="col-2-1">
+                <span className="captionLabel">
+                    Bitte wählen Sie einen Tag
+                </span>
+            </div>
+            <div className="col-2-2"></div>
+            <div className="clear-row"></div>
+            <div className="col-1-1">
+                <button
+                    className="backButton bookingFormButton"
+                    type="button"
+                    onClick={handleBackButtonClicked}
+                >
+                    <span>Zurück</span>
+                </button>
+                <button
+                    className="bookingFormButton"
+                    type="button"
+                    disabled={true}
+                >
+                    <span>Weiter</span>
+                </button>
+            </div>
+        </div>
     )
 }
 
