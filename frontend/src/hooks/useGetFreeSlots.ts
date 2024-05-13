21ee0c05477dacf55fe0c0331acb4688be7bf7d3
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-13 03:39:23 +0200

    Adds useGetFreeSlots hook

---
+++ frontend/src/hooks/useGetFreeSlots.ts
@@ -0,0 +1,32 @@
+import { useMutation } from "react-query"
+import { isAxiosError } from "axios"
+import api from "../api"
+import useSessionContext from "./useSessionContext"
+import { FilterFreeSlotDataType } from "../utils/types"
+
+
+const useGetFreeSlots = () => {
+    const { setCookieInfo, setActiveTab } = useSessionContext();
+
+    const getFreeSlots = async (data: FilterFreeSlotDataType) => {
+        const response = await api.post("/appointment/filter", data, { withCredentials: true })
+        return response.data
+    }
+
+    return useMutation({
+        mutationFn: getFreeSlots,
+        onError: (error) => {
+            if (isAxiosError(error) && error.response) {
+                console.error(error.response.data.message)
+            } else {
+                console.error("An error occured in useCreateNewUser")
+            }
+        },
+        onSuccess: ({ cookieInfo }) => {
+            setCookieInfo(cookieInfo);
+            setActiveTab("bookdate");
+        },
+    })
+}
+
+export default useGetFreeSlots
