ea40a81ca65c96ab2a9d237003e86055eb7c1efb
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-14 05:10:25 +0200

    Adds state filterTime to ServiceProvider

--- frontend/src/context/ServiceProvider.tsx
+++ frontend/src/context/ServiceProvider.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren, ReactElement, createContext, useState } from "react"
 import useGetFreeSlots from "../hooks/useGetFreeSlots"
 import { UseMutateFunction } from "react-query"
 import { FilterFreeSlotDataType, FreeTimeslotType } from "../utils/types"
+import { proposalDateRangeValues, proposalDateRangeValuesType } from "../constants"
 
 type ServiceInfoType = {
     service_name: string,
@@ -19,11 +20,23 @@ const defaultServiceInfo: ServiceInfoType = {
     employee_lastname: "",
 }
 
+export type FilterTimeType = {
+    min: proposalDateRangeValuesType,
+    max: proposalDateRangeValuesType,
+}
+
+const defaultFilterTime: FilterTimeType = {
+    min: proposalDateRangeValues[0],
+    max: proposalDateRangeValues[proposalDateRangeValues.length - 1],
+}
+
 type ServiceContextType = {
     serviceInfo: ServiceInfoType,
     setServiceInfo: React.Dispatch<React.SetStateAction<ServiceInfoType>>,
     appointment: Date | undefined,
     setAppointment: React.Dispatch<React.SetStateAction<Date | undefined>>,
+    filterTime: FilterTimeType,
+    setFilterTime: React.Dispatch<React.SetStateAction<FilterTimeType>>,
     resetServiceInfo: () => void,
     triggerGetFreeSlots: UseMutateFunction<any, unknown, FilterFreeSlotDataType, unknown>,
     isGetFreeSlotsError: boolean,
@@ -36,6 +49,8 @@ const initContextState = {
     setServiceInfo: () => { },
     appointment: undefined,
     setAppointment: () => { },
+    filterTime: defaultFilterTime,
+    setFilterTime: () => { },
     resetServiceInfo: () => { },
     triggerGetFreeSlots: () => { },
     isGetFreeSlotsError: false,
@@ -48,6 +63,7 @@ export const ServiceContext = createContext<ServiceContextType>(initContextState
 export const ServiceProvider = ({ children }: PropsWithChildren): ReactElement => {
     const [serviceInfo, setServiceInfo] = useState<ServiceInfoType>(defaultServiceInfo);
     const [appointment, setAppointment] = useState<Date | undefined>(undefined);
+    const [filterTime, setFilterTime] = useState<FilterTimeType>(defaultFilterTime);
     const {
         mutate: triggerGetFreeSlots,
         isError: isGetFreeSlotsError,
@@ -67,6 +83,8 @@ export const ServiceProvider = ({ children }: PropsWithChildren): ReactElement =
                 setServiceInfo,
                 appointment,
                 setAppointment,
+                filterTime,
+                setFilterTime,
                 resetServiceInfo,
                 triggerGetFreeSlots,
                 isGetFreeSlotsError,
