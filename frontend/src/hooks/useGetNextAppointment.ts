c8861bb6ef693dd0478b18a4dc8671a63725db00
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-14 18:25:15 +0200

    Adds useGetNextAppointments hook

---
+++ frontend/src/hooks/useGetNextAppointment.ts
@@ -0,0 +1,30 @@
+import { isAxiosError } from "axios"
+import { useQuery } from "react-query"
+import api from "../api"
+import useSessionContext from "./useSessionContext"
+
+const useGetNextAppointment = () => {
+    const { setCookieInfo } = useSessionContext();
+
+    const getNextAppointment = async () => {
+        const response = await api.get("/appointment", { withCredentials: true })
+        return response.data
+    }
+
+    return useQuery({
+        queryKey: ["nextAppointment"],
+        queryFn: getNextAppointment,
+        onSuccess: ({ cookieInfo }) => {
+            setCookieInfo(cookieInfo);
+        },
+        onError: (error) => {
+            if (isAxiosError(error) && error.response) {
+                console.error(error.response.data.message)
+            } else {
+                console.error("An error occured in useLogin")
+            }
+        },
+    })
+}
+
+export default useGetNextAppointment
