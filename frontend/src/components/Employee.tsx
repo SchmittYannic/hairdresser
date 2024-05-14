21d7e67ca665e1cdc7d36b616a71b7cd00a108be
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-14 07:38:37 +0200

    Adds Employee component

---
+++ frontend/src/components/Employee.tsx
@@ -0,0 +1,46 @@
+import { EmployeeType } from "../utils/types"
+
+
+type EmployeePropsType = {
+    employee: EmployeeType,
+}
+
+const Employee = ({ employee }: EmployeePropsType) => {
+    return (
+        <div className="col col-lg-3 col-md-6 col-sm-12 team-col">
+            <a
+                className="image-container"
+                href={employee.imglink}
+                target="_blank"
+                title={employee.imgtitle}
+            >
+                <img
+                    className="module image portrait"
+                    src={employee.img}
+                    alt={employee.firstname + " " + employee.lastname}
+                />
+            </a>
+            <div className="module text name-headline">
+                <h3 className="headline">
+                    {employee.firstname} {employee.lastname}
+                </h3>
+            </div>
+            <div className="module text desc-list">
+                <ul>
+                    {employee.description.map((desc, idx) =>
+                        <li key={idx}>
+                            {desc}
+                        </li>
+                    )}
+                </ul>
+            </div>
+            <div className="module text quote">
+                <p className="custom1">
+                    {"\"" + employee.quote + "\""}
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Employee
