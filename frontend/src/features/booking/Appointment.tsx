e38a7cc9c92cdbd63c2ec7a9b9d11baf1e6a1554
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-14 18:26:24 +0200

    Adds Appointment component

---
+++ frontend/src/features/booking/Appointment.tsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import { FaCirclePlus, FaCircleMinus } from "react-icons/fa6";
+import { AllServicesInfoType, AppointmentType, AvailableServicesKeyType, EmployeesInfoType } from "../../utils/types"
+import { allServicesInfo, employeesInfo, weekdaysAbr } from "../../constants";
+
+type AppointmentPropsType = {
+    appointment: AppointmentType,
+}
+
+const Appointment = ({ appointment }: AppointmentPropsType) => {
+    const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+
+    const service: {
+        service_label: string,
+        service_duration: number,
+    } = allServicesInfo[appointment.service_name as keyof AllServicesInfoType];
+
+    const employee: {
+        firstname: string,
+        lastname: string,
+        skills: AvailableServicesKeyType[],
+    } = employeesInfo[appointment.employee as keyof EmployeesInfoType];
+
+    const startDate = new Date(appointment.start);
+    const day = startDate.getDay();
+    const abrDay = weekdaysAbr[day];
+    const options: Intl.DateTimeFormatOptions = {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+    };
+    const dateString = startDate.toLocaleDateString("de-DE", options);
+    const startDateHours = ("0" + startDate.getHours()).slice(-2);
+    const startDateMinutes = ("0" + startDate.getMinutes()).slice(-2);
+    const startDateTime = startDateHours + ":" + startDateMinutes;
+    const endDate = new Date(appointment.end);
+    const endDateHours = ("0" + endDate.getHours()).slice(-2);
+    const endDateMinutes = ("0" + endDate.getMinutes()).slice(-2);
+    const endDateTime = endDateHours + ":" + endDateMinutes;
+
+    return (
+        <div
+            className="list-item"
+        >
+            <div className="appointmentListItem">
+                <button
+                    className="bookingButton plusButton"
+                    type="button"
+                    onClick={() => setIsDetailsOpen(!isDetailsOpen)}
+                >
+                    <span className="icon-container">
+                        {isDetailsOpen ? <FaCircleMinus aria-hidden /> : <FaCirclePlus aria-hidden />}
+                    </span>
+                </button>
+                <div className="header-container">
+                    <span className="title">
+                        {abrDay} {dateString}
+                        <br />
+                        {startDateTime} - {endDateTime}
+                    </span>
+                </div>
+                {
+                    isDetailsOpen &&
+                    <div className="details">
+                        <span className="services">
+                            <i>{appointment.service_name}</i>
+                            <br />
+                            {service.service_label}
+                            <br />
+                            Bei {employee.firstname} {employee.lastname}
+                            <br />
+                        </span>
+                        <div className="dateContainer">
+                            <span className="dateLabel">
+                                Reserviert am:
+                            </span>
+                            <span className="dateField">
+                                Mi. 14.12.2022 11:25
+                            </span>
+                        </div>
+                        <div className="clear-row"></div>
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Appointment
