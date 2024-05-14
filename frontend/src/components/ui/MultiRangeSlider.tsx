68fcdefaaa2580d1cfddc8e53283622b373ece82
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-14 02:32:44 +0200

    Removes comments

--- frontend/src/components/ui/MultiRangeSlider.tsx
+++ frontend/src/components/ui/MultiRangeSlider.tsx
@@ -105,92 +105,9 @@ const MultiRangeSlider = <T extends string | number>({
             <div className="slider">
                 <div className="slider__track" />
                 <div className="slider__range" ref={range} />
-                <div className="slider__left-value">{displayMin}</div>
-                <div className="slider__right-value">{displayMax}</div>
             </div>
         </div>
     )
-
-    // const [minVal, setMinVal] = useState(min);
-    // const [maxVal, setMaxVal] = useState(max);
-    // const minValRef = useRef<HTMLInputElement>(null);
-    // const maxValRef = useRef<HTMLInputElement>(null);
-    // const range = useRef<HTMLDivElement>(null);
-
-    // const handleMinValChange = (event: ChangeEvent<HTMLInputElement>) => {
-    //     const value = Math.min(+event.target.value, maxVal - 1);
-    //     setMinVal(value);
-    //     //event.target.value = value.toString();
-    // }
-
-    // const handleMaxValChange = (event: ChangeEvent<HTMLInputElement>) => {
-    //     const value = Math.max(+event.target.value, minVal + 1);
-    //     setMaxVal(value);
-    //     //event.target.value = value.toString();
-    // }
-
-    // const getPercent = useCallback(
-    //     (value: any) => Math.round(((value - min) / (max - min)) * 100), [min, max]
-    // );
-
-    // useEffect(() => {
-    //     if (maxValRef.current) {
-    //         const minPercent = getPercent(minVal);
-    //         const maxPercent = getPercent(+maxValRef.current.value);
-
-    //         if (range.current) {
-    //             range.current.style.left = `${minPercent}%`;
-    //             range.current.style.width = `${maxPercent - minPercent}%`;
-    //         }
-    //     }
-    // }, [minVal, getPercent]);
-
-    // useEffect(() => {
-    //     if (minValRef.current) {
-    //         const minPercent = getPercent(+minValRef.current.value);
-    //         const maxPercent = getPercent(maxVal);
-
-    //         if (range.current) {
-    //             range.current.style.width = `${maxPercent - minPercent}%`;
-    //         }
-    //     }
-    // }, [maxVal, getPercent]);
-
-    // useEffect(() => {
-    //     onChange({ min: minVal, max: maxVal });
-    // }, [minVal, maxVal, onChange]);
-
-    // return (
-    //     <>
-    //         <input
-    //             className={`thumb${minVal > max - 100 ? " thumb--zindex-5" : " thumb--zindex-3"}`}
-    //             type="range"
-    //             min={min}
-    //             max={max}
-    //             step={step}
-    //             value={minVal}
-    //             ref={minValRef}
-    //             onChange={handleMinValChange}
-    //         />
-    //         <input
-    //             className="thumb thumb--zindex-4"
-    //             type="range"
-    //             min={min}
-    //             max={max}
-    //             step={step}
-    //             value={maxVal}
-    //             ref={maxValRef}
-    //             onChange={handleMaxValChange}
-    //         />
-    // <div className="slider">
-    //     <div className="slider__track" />
-    //     <div className="slider__range" ref={range} />
-    //     <div className="slider__left-value">{minVal}</div>
-    //     <div className="slider__right-value">{maxVal}</div>
-    // </div>
-
-    //     </>
-    // )
 }
 
 export default MultiRangeSlider
