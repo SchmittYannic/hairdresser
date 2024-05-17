171c3576731c7fb6fc378143213dbea8e7be1fc6
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-17 12:03:14 +0200

    Turns off query for archivedAppointments

--- frontend/src/hooks/useGetArchivedAppointments.ts
+++ frontend/src/hooks/useGetArchivedAppointments.ts
@@ -14,6 +14,7 @@ const useGetArchivedAppointments = () => {
     return useQuery({
         queryKey: ["archivedAppointments"],
         queryFn: getArchivedAppointments,
+        enabled: false,
         onSuccess: ({ cookieInfo }) => {
             setCookieInfo(cookieInfo);
         },
