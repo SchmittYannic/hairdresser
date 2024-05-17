0affd6f6a57729f1ec32668cac1c8d538b0ab398
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-17 11:25:35 +0200

    Adds Archivedappointment Model

---
+++ backend/models/Archivedappointment.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const archivedappointmentSchema = new mongoose.Schema(
+    {
+        employee: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User",
+        },
+        customer: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User",
+        },
+        service_name: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        start: {
+            type: Date,
+            required: true,
+        },
+        end: {
+            type: Date,
+            required: true,
+            validate: {
+                validator: function (end) {
+                    // Calculate the difference in minutes between 'start' and 'end'
+                    const diffMinutes = Math.abs((end - this.start) / (1000 * 60));
+                    // Check if the difference is equal to the specified 'duration'
+                    return diffMinutes === this.duration;
+                },
+                message: () => `End time is not equal to the specified duration after the start time`
+            }
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export default mongoose.model("Archivedappointment", archivedappointmentSchema);
