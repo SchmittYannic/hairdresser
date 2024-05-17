fbc8629c30fc270d027c0330cb80a66ff845143b
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-17 11:31:57 +0200

    Adds useGetArchivedAppointments hook

---
+++ frontend/src/hooks/useGetArchivedAppointments.ts
@@ -0,0 +1,30 @@
+import { useQuery } from "react-query"
+import { isAxiosError } from "axios"
+import api from "../api"
+import useSessionContext from "./useSessionContext"
+
+const useGetArchivedAppointments = () => {
+    const { setCookieInfo } = useSessionContext()
+
+    const getArchivedAppointments = async () => {
+        const response = await api.get("/appointment/archive", { withCredentials: true })
+        return response.data
+    }
+
+    return useQuery({
+        queryKey: ["archivedAppointments"],
+        queryFn: getArchivedAppointments,
+        onSuccess: ({ cookieInfo }) => {
+            setCookieInfo(cookieInfo);
+        },
+        onError: (error) => {
+            if (isAxiosError(error) && error.response) {
+                console.error(error.response.data.message)
+            } else {
+                console.error("An error occured in useLogin")
+            }
+        },
+    })
+}
+
+export default useGetArchivedAppointments
