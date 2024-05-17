89c8da297d64d99a627ee7560cdd353b4f3ee38e
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-17 16:16:58 +0200

    Adds delete dialog to Appointment component

--- frontend/src/features/booking/Appointment.tsx
+++ frontend/src/features/booking/Appointment.tsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useState, MouseEvent, useEffect } from "react";
 import { FaCirclePlus, FaCircleMinus } from "react-icons/fa6";
+import { FaPlus } from "react-icons/fa";
+import { ImCheckmark } from "react-icons/im";
+import AsyncButton from "../../components/ui/AsyncButton";
+import Dialog from "../../components/ui/Dialog";
 import { allServicesInfo, employeesInfo, weekdaysAbr } from "../../constants";
 import { AllServicesInfoType, AppointmentType, AvailableServicesKeyType, EmployeesInfoType } from "../../utils/types"
+import useDeleteAppointment from "../../hooks/useDeleteAppointment";
 
 type AppointmentPropsType = {
     appointment: AppointmentType,
 }
 
 const Appointment = ({ appointment }: AppointmentPropsType) => {
+    const {
+        mutate: deleteAppointment,
+        isLoading: isDeleteAppointmentLoading,
+        isError: isDeleteAppointmentError,
+        reset: resetDeleteAppointmentMutation,
+    } = useDeleteAppointment();
     const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+    const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
 
     const service: {
         service_label: string,
@@ -45,6 +57,30 @@ const Appointment = ({ appointment }: AppointmentPropsType) => {
     const createdDateHours = ("0" + createdDate.getHours()).slice(-2);
     const createdDateMinutes = ("0" + createdDate.getMinutes()).slice(-2);
     const createdDateTime = createdDateHours + ":" + createdDateMinutes;
+
+    const now = new Date();
+    const diffNowAndAppointment = startDate.getTime() - now.getTime();
+    const twentyFourHoursInMs = 24 * 60 * 60 * 1000;
+    const isWithinNext24Hours = diffNowAndAppointment >= 0 && diffNowAndAppointment < twentyFourHoursInMs;
+
+    const handleDeleteAppointmentClicked = (e: MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault();
+        setIsDeleteDialogOpen(true);
+    };
+
+    const handleDeleteYesClicked = () => {
+        deleteAppointment(appointment._id);
+    };
+
+    const handleDeleteNoClicked = () => {
+        setIsDeleteDialogOpen(false);
+    };
+
+    useEffect(() => {
+        if (!isDeleteDialogOpen) {
+            resetDeleteAppointmentMutation();
+        }
+    }, [isDeleteDialogOpen])
 
     return (
         <div className="list-item">
@@ -94,12 +130,77 @@ const Appointment = ({ appointment }: AppointmentPropsType) => {
                         <div>
                             <a
                                 href=""
+                                onClick={handleDeleteAppointmentClicked}
                             >
                                 Termin stornieren
                             </a>
                         </div>
                         <div className="clear-row"></div>
                     </div>
+                }
+                {
+                    isDeleteDialogOpen &&
+                    <Dialog setDialog={setIsDeleteDialogOpen}>
+                        <div className="dialog__caption ">
+                            Termin löschen
+                        </div>
+                        <div className="dialog__content">
+                            <span className="label">
+                                Wollen Sie diesen Termin löschen?
+                            </span>
+                            {
+                                isWithinNext24Hours &&
+                                <>
+                                    <span
+                                        className="label"
+                                        style={{
+                                            whiteSpace: "wrap",
+                                            maxWidth: "270px",
+                                            display: "block",
+                                        }}>
+                                        Termine innerhalb der nächsten 24 Stunden können nicht kostenfrei abgesagt werden.
+                                    </span>
+                                    <span className="label">
+                                        Dennoch fortfahren?
+                                    </span>
+                                </>
+                            }
+                            {
+                                isDeleteAppointmentError &&
+                                <span className="error-msg" role="alert">
+                                    Fehler bei Löschung des Termins.
+                                </span>
+                            }
+                        </div>
+                        <div className="dialog__button__container">
+                            <AsyncButton
+                                className="bookingFormButton"
+                                type="button"
+                                onClick={handleDeleteYesClicked}
+                                isLoading={isDeleteAppointmentLoading}
+                                disabled={isDeleteAppointmentLoading}
+                            >
+                                <span className="icon-container">
+                                    <ImCheckmark aria-hidden />
+                                </span>
+                                <span className="icon-gap">
+                                    ja
+                                </span>
+                            </AsyncButton>
+                            <button
+                                className="bookingFormButton"
+                                type="button"
+                                onClick={handleDeleteNoClicked}
+                            >
+                                <span className="icon-container">
+                                    <FaPlus aria-hidden style={{ transform: "rotate(45deg)" }} />
+                                </span>
+                                <span className="icon-gap">
+                                    nein
+                                </span>
+                            </button>
+                        </div>
+                    </Dialog>
                 }
             </div>
         </div>
