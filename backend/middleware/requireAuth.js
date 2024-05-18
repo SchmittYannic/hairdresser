21010cbde9e6258182b54b834ca0b654a8c45b3d
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-18 23:19:51 +0200

    Tests

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,4 +1,4 @@
-const requireAuth = (req, res, next) => {
+const requireAuth = async (req, res, next) => {
     // console.log("session: ", req.session)
     // console.log("user: ", req.session.user)
     // console.log("session id: ", req.session.sessionID)
