ec506002768aedcfb548c965bb61d933141495d8
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-19 00:35:53 +0200

    Removes console.logs

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,8 +1,4 @@
 const requireAuth = (req, res, next) => {
-    console.log("session: ", req.session)
-    console.log("user: ", req.session.user)
-    console.log("session id: ", req.session.sessionID)
-
     if (req.session && req.session.user) {
         next();
     } else {
