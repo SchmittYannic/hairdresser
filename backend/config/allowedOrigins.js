0fedc9e5c5e7cefddd92f083154b880a24f22b95
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-18 20:30:32 +0200

    Tests

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     "http://localhost:5173",
     "https://hairdresser-363l.onrender.com",
-    "https://www.project-domain.de",
+    "https://project-domain.de",
 ];
 
 export default allowedOrigins;
