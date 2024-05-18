9b653a705659793a4f4536861d47ca5760a3fbbf
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-18 22:19:56 +0200

    Tests

--- backend/server.js
+++ backend/server.js
@@ -32,16 +32,17 @@ app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 app.use(express.json());
+app.set("trust proxy", 1);
 app.use(session(sessionConfig(db)));
 cron.schedule("0 0 * * * *", async () => await moveExpiredAppointments(db));
 //cron.schedule("*/10 * * * * *", async () => await insertFakeData(10))
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Set-Cookie');
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Origin', req.headers.origin);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Set-Cookie');
+//     next();
+// });
 // app.options('*', function (req, res) {
 //     //res.header('Access-Control-Allow-Origin', '*');
 //     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
