76f34964d59035db9bff6f0a165a89d6d2a629af
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-19 17:24:57 +0200

    Adds Codesplitting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,29 @@
+import { ComponentType, Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom"
 import PersistLogin from "./features/auth/PersistLogin"
 import Layout from "./components/Layout"
-import Frontpage from "./components/Frontpage"
-import Bookingpage from "./components/Bookingpage"
-import Stylingpage from "./components/Stylingpage"
-import Colourpage from "./components/Colourpage"
-import Salonpage from "./components/Salonpage"
-import Teampage from "./components/Teampage"
-import Jobspage from "./components/Jobspage"
-import Kontaktpage from "./components/Kontaktpage"
-import Impressum from "./components/Impressum"
-import Datenschutz from "./components/Datenschutz"
-import Booking from "./features/booking/Booking"
+
+const withSuspense = <P extends object>(
+	Component: ComponentType<P>
+): React.FC<P> => (props) => (
+	<Suspense fallback={<div>Loading...</div>}>
+		<Component {...props} />
+	</Suspense>
+);
+
+const Frontpage = withSuspense(lazy(() => import("./components/Frontpage" /* webpackChunkName: "Frontpage" */)));
+const Bookingpage = withSuspense(lazy(() => import("./components/Bookingpage" /* webpackChunkName: "Bookingpage" */)));
+const Stylingpage = withSuspense(lazy(() => import("./components/Stylingpage" /* webpackChunkName: "Stylingpage" */)));
+const Colourpage = withSuspense(lazy(() => import("./components/Colourpage" /* webpackChunkName: "Colourpage" */)));
+const Salonpage = withSuspense(lazy(() => import("./components/Salonpage" /* webpackChunkName: "Salonpage" */)));
+const Teampage = withSuspense(lazy(() => import("./components/Teampage" /* webpackChunkName: "Teampage" */)));
+const Jobspage = withSuspense(lazy(() => import("./components/Jobspage" /* webpackChunkName: "Jobspage" */)));
+const Kontaktpage = withSuspense(lazy(() => import("./components/Kontaktpage" /* webpackChunkName: "Kontaktpage" */)));
+const Impressum = withSuspense(lazy(() => import("./components/Impressum" /* webpackChunkName: "Impressum" */)));
+const Datenschutz = withSuspense(lazy(() => import("./components/Datenschutz" /* webpackChunkName: "Datenschutz" */)));
+const Booking = withSuspense(lazy(() => import("./features/booking/Booking" /* webpackChunkName: "Booking" */)));
 
 const App = () => {
-
 	return (
 		<Routes>
 			<Route element={<PersistLogin />}>
