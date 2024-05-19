0e981733902f3643d7db6b82aa528e07709fae3a
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-19 17:05:06 +0200

    Fixes Codesplitting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,38 @@
-import { lazy, Suspense, ComponentType } from "react"
+import { lazy, Suspense } from "react"
 import { Routes, Route } from "react-router-dom"
 import PersistLogin from "./features/auth/PersistLogin"
 import Layout from "./components/Layout"
 
-const withSuspense = <P extends object>(
-	Component: ComponentType<P>
-): React.FC<P> => (props) => (
-	<Suspense fallback={<div>Loading...</div>}>
-		<Component {...props} />
-	</Suspense>
-);
-
-const Frontpage = withSuspense(lazy(() => import("./components/Frontpage" /* webpackChunkName: "Frontpage" */)));
-const Bookingpage = withSuspense(lazy(() => import("./components/Bookingpage" /* webpackChunkName: "Bookingpage" */)));
-const Stylingpage = withSuspense(lazy(() => import("./components/Stylingpage" /* webpackChunkName: "Stylingpage" */)));
-const Colourpage = withSuspense(lazy(() => import("./components/Colourpage" /* webpackChunkName: "Colourpage" */)));
-const Salonpage = withSuspense(lazy(() => import("./components/Salonpage" /* webpackChunkName: "Salonpage" */)));
-const Teampage = withSuspense(lazy(() => import("./components/Teampage" /* webpackChunkName: "Teampage" */)));
-const Jobspage = withSuspense(lazy(() => import("./components/Jobspage" /* webpackChunkName: "Jobspage" */)));
-const Kontaktpage = withSuspense(lazy(() => import("./components/Kontaktpage" /* webpackChunkName: "Kontaktpage" */)));
-const Impressum = withSuspense(lazy(() => import("./components/Impressum" /* webpackChunkName: "Impressum" */)));
-const Datenschutz = withSuspense(lazy(() => import("./components/Datenschutz" /* webpackChunkName: "Datenschutz" */)));
-const Booking = withSuspense(lazy(() => import("./features/booking/Booking" /* webpackChunkName: "Booking" */)));
-
+const Frontpage = lazy(() => import("./components/Frontpage" /* webpackChunkName: "Frontpage" */));
+const Bookingpage = lazy(() => import("./components/Bookingpage" /* webpackChunkName: "Bookingpage" */));
+const Stylingpage = lazy(() => import("./components/Stylingpage" /* webpackChunkName: "Stylingpage" */));
+const Colourpage = lazy(() => import("./components/Colourpage" /* webpackChunkName: "Colourpage" */));
+const Salonpage = lazy(() => import("./components/Salonpage" /* webpackChunkName: "Salonpage" */));
+const Teampage = lazy(() => import("./components/Teampage" /* webpackChunkName: "Teampage" */));
+const Jobspage = lazy(() => import("./components/Jobspage" /* webpackChunkName: "Jobspage" */));
+const Kontaktpage = lazy(() => import("./components/Kontaktpage" /* webpackChunkName: "Kontaktpage" */));
+const Impressum = lazy(() => import("./components/Impressum" /* webpackChunkName: "Impressum" */));
+const Datenschutz = lazy(() => import("./components/Datenschutz" /* webpackChunkName: "Datenschutz" */));
+const Booking = lazy(() => import("./features/booking/Booking" /* webpackChunkName: "Booking" */));
 
 const App = () => {
+
 	return (
 		<Routes>
 			<Route element={<PersistLogin />}>
-				<Route path="/terminbuch/termine" element={<Booking />} />
+				<Route path="/terminbuch/termine" element={<Suspense><Booking /></Suspense>} />
 			</Route>
 			<Route path="/" element={<Layout />} >
-				<Route index element={<Frontpage />} />
-				<Route path="terminbuchung" element={<Bookingpage />} />
-				<Route path="styling" element={<Stylingpage />} />
-				<Route path="colour" element={<Colourpage />} />
-				<Route path="salon" element={<Salonpage />} />
-				<Route path="team" element={<Teampage />} />
-				<Route path="jobs" element={<Jobspage />} />
-				<Route path="kontakt" element={<Kontaktpage />} />
-				<Route path="impressum" element={<Impressum />} />
-				<Route path="datenschutzerklaerung" element={<Datenschutz />} />
+				<Route index element={<Suspense><Frontpage /></Suspense>} />
+				<Route path="terminbuchung" element={<Suspense><Bookingpage /></Suspense>} />
+				<Route path="styling" element={<Suspense><Stylingpage /></Suspense>} />
+				<Route path="colour" element={<Suspense><Colourpage /></Suspense>} />
+				<Route path="salon" element={<Suspense><Salonpage /></Suspense>} />
+				<Route path="team" element={<Suspense><Teampage /></Suspense>} />
+				<Route path="jobs" element={<Suspense><Jobspage /></Suspense>} />
+				<Route path="kontakt" element={<Suspense><Kontaktpage /></Suspense>} />
+				<Route path="impressum" element={<Suspense><Impressum /></Suspense>} />
+				<Route path="datenschutzerklaerung" element={<Suspense><Datenschutz /></Suspense>} />
 			</Route>
 		</Routes>
 	)
