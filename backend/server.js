567ecc78099a576419f4199be3e0e2565377ea87
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-25 15:55:59 +0200

    Adds trigger to delete all appointments and archived appointments of a user when he is deleted

--- backend/server.js
+++ backend/server.js
@@ -49,10 +49,30 @@ app.use(errorHandler);
 db.once("open", () => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+    // stream watching if document from users collection is deleted
+    const userDeleteStream = db.collection("users").watch([
+        { $match: { operationType: "delete" } }
+    ]);
+
+    // whenever a user gets deleted trigger cascade delete for all associated active and archived appointments
+    userDeleteStream.on("change", async (change) => {
+        try {
+            const deletedUserId = change.documentKey._id;
+
+            const appointmentsCollection = db.collection("appointments");
+            const archivedAppointmentsCollection = db.collection("archivedappointments");
+
+            await appointmentsCollection.deleteMany({ customer: deletedUserId });
+            await archivedAppointmentsCollection.deleteMany({ customer: deletedUserId });
+        } catch (err) {
+            logEvents(`"userDeleteStream": ${change.documentKey._id}\t${err}`, "streamErrLog.log");
+        }
+    })
 });
 
 // listen to error
 db.on("error", err => {
     console.log(err);
-    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log');
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, "mongoErrLog.log");
 });
