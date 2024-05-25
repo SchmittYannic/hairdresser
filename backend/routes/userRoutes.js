dd17aafae8117e2c7037bbf91b30fcb4a8bc3506
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-25 15:55:21 +0200

    Adds deleteUser to /users/delete post endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createNewUser, updateUser } from "../controllers/usersController.js";
+import { createNewUser, updateUser, deleteUser } from "../controllers/usersController.js";
 import requireAuth from "../middleware/requireAuth.js"
 import { signupLimiter } from "../middleware/rateLimiters.js";
 
@@ -8,5 +8,8 @@ const router = express.Router();
 router.route("/")
     .post(signupLimiter, createNewUser)
     .patch(requireAuth, updateUser)
+
+router.route("/delete")
+    .post(requireAuth, deleteUser)
 
 export default router;
