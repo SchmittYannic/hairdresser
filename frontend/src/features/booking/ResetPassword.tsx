c137eb4412e6ab51ba7323182c512888cdc8a951
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-26 18:43:26 +0200

    Updates ResetPassword component with api call

--- frontend/src/features/booking/ResetPassword.tsx
+++ frontend/src/features/booking/ResetPassword.tsx
@@ -1,53 +1,133 @@
-import useSessionContext from "../../hooks/useSessionContext"
+import { useEffect } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { isAxiosError } from "axios";
+import useSessionContext from "../../hooks/useSessionContext";
+import useResetPasswordEmail from "../../hooks/useResetPasswordEmail";
+import { resetpasswordschema } from "../../validation/Userschema";
+import AsyncButton from "../../components/ui/AsyncButton";
 
 const ResetPassword = () => {
 
     const { setActiveTab } = useSessionContext();
 
+    const {
+        mutate: triggerResetPasswordEmail,
+        isLoading: isResetPasswordLoading,
+        isSuccess: isResetPasswordSuccess,
+        isError: isResetPasswordError,
+        error: errorResetPassword,
+        data: dataResetPassword,
+        reset: resetResetPasswordMutation,
+    } = useResetPasswordEmail();
+
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors },
+        reset: resetForm,
+    } = useForm<{ email: string }>({
+        resolver: yupResolver(resetpasswordschema),
+    });
+
+    const onSubmit: SubmitHandler<{ email: string }> = (data) => {
+        triggerResetPasswordEmail(data);
+    };
+
+    useEffect(() => {
+        if (!isResetPasswordError) return
+        if (!isAxiosError(errorResetPassword)) return
+        if (!errorResetPassword.response) return
+        if (!errorResetPassword.response.data.context) return
+        if (!errorResetPassword.response.data.context.label) return
+
+        setError(errorResetPassword.response.data.context.label, {
+            message: errorResetPassword.response.data.message,
+        })
+    }, [setError, isResetPasswordError]);
+
+    useEffect(() => {
+        if (!isResetPasswordSuccess) return
+        resetForm({
+            email: "",
+        })
+    }, [isResetPasswordSuccess]);
+
     return (
         <div className="page">
-            <div className="col-1-1">
-                <span className="infoLabel">
-                    Wenn Sie Ihr Passwort nicht mehr wissen, können Sie es hier zurücksetzen. Geben Sie dazu Ihre E-Mail-Adresse ein.
-                </span>
-                <form className="bookingForm">
-                    <div className="bookingFormRow">
-                        <label
-                            htmlFor="resetEmail"
-                            className="bookingFormLabel"
-                        >
-                            E-Mail-Adresse
-                        </label>
-                        <div className="bookingFormField">
-                            <div className="textfield">
-                                <input
-                                    id="resetEmail"
-                                    autoCapitalize="none"
-                                    type="text"
-                                    maxLength={80}
-                                />
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="col-1-1">
+                    <span className="infoLabel">
+                        Wenn Sie Ihr Passwort nicht mehr wissen, können Sie es hier zurücksetzen. Geben Sie dazu Ihre E-Mail-Adresse ein.
+                    </span>
+                    <div className="bookingForm">
+                        <div className="bookingFormRow">
+                            <label
+                                htmlFor="resetEmail"
+                                className="bookingFormLabel"
+                            >
+                                E-Mail-Adresse
+                            </label>
+                            <div className="bookingFormField">
+                                <div
+                                    className={`textfield${errors.email ? " error" : ""}`}
+                                >
+                                    <input
+                                        id="resetEmail"
+                                        autoCapitalize="none"
+                                        type="text"
+                                        maxLength={80}
+                                        {...register("email")}
+                                    />
+                                </div>
+                                {
+                                    errors.email &&
+                                    <span className="error-label" role="alert">
+                                        {errors.email.message}
+                                    </span>
+                                }
                             </div>
                         </div>
                     </div>
-                </form>
-            </div>
-            <div className="clear-row"></div>
-            <div className="col-1-1">
-                <button
-                    className="backButton bookingFormButton"
-                    type="button"
-                    onClick={() => setActiveTab("login")}
-                >
-                    <span>Zurück</span>
-                </button>
-                <button
-                    className="bookingFormButton"
-                    type="button"
-                >
-                    <span>Zurücksetzen</span>
-                </button>
+                </div>
+                {
+                    isResetPasswordSuccess &&
+                    <div className="col-1-1">
+                        <span className="success-msg" role="alert">
+                            {dataResetPassword.message}
+                        </span>
+                    </div>
+                }
+                {
+                    isResetPasswordError && isAxiosError(errorResetPassword) && errorResetPassword.response && errorResetPassword.response.data.context === undefined &&
+                    <div className="col-1-1">
+                        <span className="error-msg" role="alert">
+                            Etwas ist schiefgelaufen. Versuchen Sie es später erneut.
+                        </span>
+                    </div>
+                }
                 <div className="clear-row"></div>
-            </div>
+                <div className="col-1-1">
+                    <button
+                        className="backButton bookingFormButton"
+                        type="button"
+                        onClick={() => setActiveTab("login")}
+                    >
+                        <span>Zurück</span>
+                    </button>
+                    <AsyncButton
+                        className="bookingFormButton"
+                        type="submit"
+                        onClick={() => resetResetPasswordMutation()}
+                        isLoading={isResetPasswordLoading}
+                        disabled={isResetPasswordLoading}
+                    >
+                        <span>Zurücksetzen</span>
+                    </AsyncButton>
+                    <div className="clear-row"></div>
+                </div>
+            </form>
         </div>
     )
 }
