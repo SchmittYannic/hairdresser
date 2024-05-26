22992713c50e8109719092d1dac86d50cac8a1e6
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-05-26 18:42:02 +0200

    Adds resetPasswordEmail to route /auth/reset post endpoint

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, loggedIn } from "../controllers/authController.js";
+import { login, logout, loggedIn, resetPasswordEmail } from "../controllers/authController.js";
 import requireAuth from "../middleware/requireAuth.js"
 
 const router = express.Router();
@@ -8,5 +8,8 @@ router.route("/")
     .get(loggedIn)
     .post(login)
     .delete(requireAuth, logout)
+
+router.route("/reset")
+    .post(resetPasswordEmail)
 
 export default router;
