b516d2a3ae80be8a0ead8ff74cf5f98ab950f33e
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-07-06 20:51:01 +0200

    Updates requireAuth to check for CookieConsent

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,5 +1,23 @@
 const requireAuth = (req, res, next) => {
     if (req.session && req.session.user) {
+        const { CookieConsent } = req.cookies;
+
+        if (!CookieConsent) {
+            req.session.destroy();
+
+            return res.status(401).json({ message: "Bitte erlauben Sie essentielle Cookies", context: { key: "CookieConsent" } });
+        }
+
+        const CookieConsentParsed = JSON.parse(CookieConsent);
+
+        if (!CookieConsentParsed.necessary) {
+
+            req.session.destroy();
+
+            res.clearCookie("CookieConsent");
+            return res.status(401).json({ message: "Bitte erlauben Sie essentielle Cookies", context: { key: "CookieConsent" } });
+        }
+
         next();
     } else {
         res.status(401).json({ message: "Unauthorized" });
