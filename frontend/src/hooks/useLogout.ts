11ac8c8bf87ddf840e3db28e74355577eec119ea
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-04-12 17:35:45 +0200

    Testing issues with useLogout on mobile

--- frontend/src/hooks/useLogout.ts
+++ frontend/src/hooks/useLogout.ts
@@ -14,21 +14,36 @@ const useLogout = () => {
     return useMutation({
         mutationFn: logout,
         onSettled: () => {
-            resetState();
+            console.log("[Logout] onSettled");
+            try {
+                resetState();
+                console.log("[Logout] resetState complete");
+            } catch (e) {
+                console.error("[Logout] Error in resetState", e);
+            }
 
             try {
-                if ("BroadcastChannel" in window) {
-                    console.log("User Agent:", navigator.userAgent);
-                    console.log("BroadcastChannel in window?", "BroadcastChannel" in window);
-                    const bc = new BroadcastChannel("auth");
-                    bc.postMessage({ type: "LOGOUT" });
-                    bc.close();
-                } else {
-                    console.warn("BroadcastChannel not supported on this browser");
-                }
+                console.log("[Logout] post-resetState logic");
+                const bc = new BroadcastChannel("auth");
+                bc.postMessage({ type: "LOGOUT" });
+                bc.close();
             } catch (e) {
-                console.error("BroadcastChannel error during logout", e);
+                console.error("[Logout] post-resetState error", e);
             }
+
+            // try {
+            //     if ("BroadcastChannel" in window) {
+            //         console.log("User Agent:", navigator.userAgent);
+            //         console.log("BroadcastChannel in window?", "BroadcastChannel" in window);
+            //         const bc = new BroadcastChannel("auth");
+            //         bc.postMessage({ type: "LOGOUT" });
+            //         bc.close();
+            //     } else {
+            //         console.warn("BroadcastChannel not supported on this browser");
+            //     }
+            // } catch (e) {
+            //     console.error("BroadcastChannel error during logout", e);
+            // }
         },
         onError: (error) => {
             if (isAxiosError(error) && error.response) {
