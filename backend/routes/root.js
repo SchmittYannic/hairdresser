cbb9bb62b34257aff0b1f7e49c8647cdd877945e
SchmittYannic <schmitt.yannic@web.de>
2026-03-01 19:45:46 +0100

    Fixes root regex

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -6,7 +6,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const router = express.Router();
 // get allows regex in express
 // this will only match if the requested route is only a slash OR /index with optional .html
-router.get('^/$|/index(.html)?', (req, res) => {
+router.get(/^\/$|\/index(.html)?/, (req, res) => {
     // sends the index.html located in views folder as response
     res.sendFile(path.join(__dirname, "..", "views", "index.html"));
 });
